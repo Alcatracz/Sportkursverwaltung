4abee0bc1e74720421541436019cdb2af6462c4c
arljba <arno7@web.de>
2017-11-28 23:58:05 +0100

    jpa 2
    2

--- Sportkursverwaltung/src/controller/LoginController.java
+++ Sportkursverwaltung/src/controller/LoginController.java
@@ -1,14 +1,18 @@
 package controller;
 
 import model.User;
+import ejb.CustomerEJB;
+import entitys.Customer;
 
 public class LoginController {
 
 	private User user;
+	CustomerEJB cejb;
 	
     public String login(){
         // Image here a database access to validate the users
-        if (user.getName().equalsIgnoreCase("tester") && user.getPassword().equalsIgnoreCase("tester")){
+    	System.out.println("login");
+        if (cejb.isValidUser(user.getName(), user.getPassword())){
             return "customer";
         } else if(user.getName().equalsIgnoreCase("trainer") && user.getPassword().equalsIgnoreCase("trainer")){
         	return "trainer";
