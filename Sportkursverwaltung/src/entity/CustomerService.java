6ec7b8514f668e9eea5075bf6fc8ce5d8ee61505
arljba <arno7@web.de>
2017-12-01 14:25:18 +0100

    emservice ex
    <>

---
+++ Sportkursverwaltung/src/entity/CustomerService.java
@@ -0,0 +1,43 @@
+package entity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+public class CustomerService {
+
+//    Use this as followed 	
+//    EntityManagerFactory emf = Persistence.createEntityManagerFactory("Sportverwaltung");
+//    EntityManager em = emf.createEntityManager();
+//    CustomerService service = new CustomerService(em);
+//    em.getTransaction().begin();
+//    Customer emp = service.createCustomer(var,var,var,var,var);
+//    em.getTransaction().commit();
+	
+	private EntityManager em;
+
+	public CustomerService(EntityManager em) {
+		this.em = em;
+	}
+
+	public Customer createCustomer(Long id, String email, String firstname, String lastname, String password) {
+		Customer emp = new Customer();
+		emp.setId(id);
+		emp.setEmail(email);
+		emp.setFirstname(firstname);
+		emp.setLastname(lastname);
+		emp.setPassword(password);
+		em.persist(emp);
+		return emp;
+	}
+
+	public void removeCustomer(int id) {
+		Customer emp = findCustomer(id);
+		if (emp != null) {
+			em.remove(emp);
+		}
+	}
+
+	public Customer findCustomer(int id) {
+		return em.find(Customer.class, id);
+	}
+}
