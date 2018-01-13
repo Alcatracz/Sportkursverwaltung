f6e8bcd29a417d9673508a4ec23c58213438c4ac
Alcatracz <jan.ole.clausen78@googlemail.com>
2018-01-13 23:03:50 +0100

    play des jahrtausends

---
+++ Sportkursverwaltung/src/test/AnotherTest.java
@@ -0,0 +1,60 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import model.ProfilDatenModel;
+
+public class AnotherTest {
+
+	public static void main(String args[]) {
+		Connection c = null;
+	      PreparedStatement pstmt = null;
+	      String sql="SELECT * FROM mitglied WHERE id=?;";
+	      
+	      try {
+	         Class.forName("org.postgresql.Driver");
+	         c = DriverManager
+	            .getConnection("jdbc:postgresql://localhost:5432/Terminverwaltung",
+	            "postgres", "postgres");
+	         
+	         c.setAutoCommit(false);
+	         System.out.println("Opened database successfully");
+	        
+	         pstmt = c.prepareStatement(sql);
+	         pstmt.setInt(1, 1);
+	         
+	         
+	         ResultSet rs = pstmt.executeQuery();
+	         
+	         while(rs.next()) {
+	        	ProfilDatenModel profilDaten = new ProfilDatenModel();
+	        	
+	        	profilDaten.setVorname(rs.getString("vorname"));
+	        	profilDaten.setNachname(rs.getString("nachname"));
+	        	profilDaten.setEmail(rs.getString("email"));
+	        	profilDaten.setPasswort(rs.getString("passwort"));
+	        	profilDaten.setIstTrainer(rs.getBoolean("isttrainer"));
+	        	profilDaten.setIstBuchungsbestaetigung(rs.getBoolean("istbuchungsbestaetigung"));
+	        	profilDaten.setIstTerminerinnerung(rs.getBoolean("istterminerinnerung"));
+	        	profilDaten.setTerminerinnerungZeit(rs.getInt("terminerinnerungzeit"));
+	        	
+	        
+	        	System.out.println("Name: "+profilDaten.getVorname());
+	        	
+	         }
+	         rs.close();
+	         pstmt.close();
+	         c.close();
+	         
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	         System.err.println(e.getClass().getName()+": "+e.getMessage());
+	         System.exit(0);
+	      }
+	     
+	      System.out.println("Operation done successfully");
+	}
+}
