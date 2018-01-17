d1d1791061b728c86256eccf71ee69a95d79c9a1
arljba <arno7@web.de>
2018-01-17 01:09:20 +0100

    TimeManagement 2
    <>

--- Sportkursverwaltung/src/controller/TimeManagement.java
+++ Sportkursverwaltung/src/controller/TimeManagement.java
@@ -5,6 +5,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Period;
@@ -14,8 +17,9 @@ import java.util.List;
 
 import javax.ejb.*;
 
+import model.AktivitaetModel;
 import model.KursTerminModel;
-import model.TerminModel;
+import model.TerminModelTimeManagement;
 
 @Stateless
 public class TimeManagement {
@@ -23,21 +27,24 @@ public class TimeManagement {
 
 	private String dbUser = "postgres";
 	private String dbPasswort = "postgres";
-	private List<KursTerminModel> trainerTermine = new ArrayList<KursTerminModel>();
+	private List<TerminModelTimeManagement> trainerTermine = new ArrayList<TerminModelTimeManagement>();
+	LocalDate localDate;
 
 	
 	@Schedule(hour="*",minute="*",second="*/10",persistent=false)
 	public void test() {
 		ladeTrainerTermine();
 		trainerTermine.size();
-	       LocalDate localDate = LocalDate.now().minusDays(1);
+	       localDate = LocalDate.now().minusDays(1);
 	       DateTimeFormatter.ofPattern("yyy-MM-dd").format(localDate);
 	       System.out.println("Current Date"+localDate.toString());
 		for(int i=0;i<trainerTermine.size();i++) {
 			String date1 = trainerTermine.get(i).getDatum();
 			System.out.println(date1);
 			if(date1.equals(localDate.toString())){
-				System.out.println("Wrong order");
+				if(trainerTermine.get(i).isIstWoechentlich()) {
+					erstelleNeuenTermin(trainerTermine.get(i));
+				}
 				loescheAltenTermin(trainerTermine.get(i));
 			}
 		}
@@ -45,7 +52,7 @@ public class TimeManagement {
 	}
 	
 	
-	private void loescheAltenTermin(KursTerminModel termin) {
+	private void loescheAltenTermin(TerminModelTimeManagement termin) {
 		Connection c = null;
 	      PreparedStatement pstmt = null;
 	      String sql = "DELETE FROM termin WHERE id=?";
@@ -104,7 +111,7 @@ public class TimeManagement {
 			         ResultSet rs = stmt.executeQuery(sql);
 			         while(rs.next()) {
 		
-			        	KursTerminModel terminModel = new KursTerminModel();
+			        	TerminModelTimeManagement terminModel = new TerminModelTimeManagement();
 			        	terminModel.setId(rs.getInt("id"));
 			        	terminModel.setName(rs.getString("name"));
 			        	terminModel.setTrainer(rs.getString("trainer"));
@@ -133,6 +140,56 @@ public class TimeManagement {
 			      System.out.println("Operation done successfully");
 			      System.out.println("LISTE: "+trainerTermine.size());
 		
+	}
+	
+	public String erstelleNeuenTermin(TerminModelTimeManagement termin) {
+		System.out.println("speicherNeuenTermin");
+		
+		 Connection c = null;
+	      PreparedStatement pstmt = null;
+	      String sql = "INSERT INTO termin (startuhrzeit,enduhrzeit,datum,istwoechentlich,buchbarab,buchbarbis,stornierbarbis,aktivitaetid,istbuchbar,iststornierbar)"
+	      		+ " VALUES (?,?,?,?,?,?,?,?,?,?);";
+	      
+	      try {
+	         Class.forName("org.postgresql.Driver");
+	         c = DriverManager
+	            .getConnection("jdbc:postgresql://localhost:5432/Terminverwaltung",
+	            		dbUser, dbPasswort);
+	         
+	         c.setAutoCommit(true);
+	         System.out.println("Opened database successfully");
+	        DateFormat timeformatter = new SimpleDateFormat("HH:MM:SS");
+	        
+	         pstmt = c.prepareStatement(sql);
+	         
+	         pstmt.setTime(1, new Time(timeformatter.parse(termin.getStartUhrzeit()).getTime()));
+	         pstmt.setTime(2, new Time(timeformatter.parse(termin.getEndUhrzeit()).getTime()));
+	         pstmt.setDate(3, java.sql.Date.valueOf(localDate.plusDays(14)));
+	         pstmt.setBoolean(4, termin.isIstWoechentlich());
+	         pstmt.setInt(5, termin.getBuchbarAb()); 
+	         pstmt.setInt(6, termin.getBuchbarBis());
+	         pstmt.setInt(7, termin.getStornierbarBis());
+	         pstmt.setInt(8, termin.getAktivitaetid());
+	         pstmt.setBoolean(9, true);
+	         pstmt.setBoolean(10, true);
+	         
+	         
+	    
+	         pstmt.executeUpdate();
+	  
+	         pstmt.close();
+	         c.close();
+	         
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	         System.err.println(e.getClass().getName()+": "+e.getMessage());
+	         System.exit(0);
+	      }
+	      trainerTermine.add(termin);
+	     termin = new TerminModelTimeManagement();
+	      System.out.println("Operation done successfully");
+	      //ladeTermine(currentAktivitaetId);
+		return null;
 	}
 	
 	
---
+++ Sportkursverwaltung/src/model/TerminModelTimeManagement.java
@@ -0,0 +1,171 @@
+package model;
+
+public class TerminModelTimeManagement {
+
+	
+	private String actionName;
+	private boolean gesperrt;
+	
+	private int id;	
+	private String name;
+	private String trainer;
+	private String beschreibung;
+	private String datum;
+	private String startUhrzeit;
+	private String endUhrzeit;
+	private int buchbarAb=14;
+	private int buchbarBis=1;
+	private int stornierbarBis=1;
+	private boolean istBuchbar;
+	private boolean istStornierbar;
+	private boolean istWoechentlich;
+	private int aktivitaetid;
+	private int dauer;
+	
+	public TerminModelTimeManagement() {
+		this.buchbarAb = 14;
+		this.buchbarBis = 1;
+		this.stornierbarBis = 1;
+	}
+	
+	//------------------------------------------------------------------
+	//------------GETTER UND SETTER-------------------------------------
+	//------------------------------------------------------------------
+
+	public String getActionName() {
+		return actionName;
+	}
+
+	public void setActionName(String actionName) {
+		this.actionName = actionName;
+	}
+
+	public boolean isGesperrt() {
+		return gesperrt;
+	}
+
+	public void setGesperrt(boolean gesperrt) {
+		this.gesperrt = gesperrt;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(String trainer) {
+		this.trainer = trainer;
+	}
+
+	public String getBeschreibung() {
+		return beschreibung;
+	}
+
+	public void setBeschreibung(String beschreibung) {
+		this.beschreibung = beschreibung;
+	}
+
+	public String getDatum() {
+		return datum;
+	}
+
+	public void setDatum(String datum) {
+		this.datum = datum;
+	}
+
+	public String getStartUhrzeit() {
+		return startUhrzeit;
+	}
+
+	public void setStartUhrzeit(String startUhrzeit) {
+		this.startUhrzeit = startUhrzeit;
+	}
+
+	public String getEndUhrzeit() {
+		return endUhrzeit;
+	}
+
+	public void setEndUhrzeit(String endUhrzeit) {
+		this.endUhrzeit = endUhrzeit;
+	}
+
+	public int getBuchbarAb() {
+		return buchbarAb;
+	}
+
+	public void setBuchbarAb(int buchbarAb) {
+		this.buchbarAb = buchbarAb;
+	}
+
+	public int getBuchbarBis() {
+		return buchbarBis;
+	}
+
+	public void setBuchbarBis(int buchbarBis) {
+		this.buchbarBis = buchbarBis;
+	}
+
+	public int getStornierbarBis() {
+		return stornierbarBis;
+	}
+
+	public void setStornierbarBis(int stornierbarBis) {
+		this.stornierbarBis = stornierbarBis;
+	}
+
+	public boolean isIstBuchbar() {
+		return istBuchbar;
+	}
+
+	public void setIstBuchbar(boolean istBuchbar) {
+		this.istBuchbar = istBuchbar;
+	}
+
+	public boolean isIstStornierbar() {
+		return istStornierbar;
+	}
+
+	public void setIstStornierbar(boolean istStornierbar) {
+		this.istStornierbar = istStornierbar;
+	}
+
+	public int getDauer() {
+		return dauer;
+	}
+
+	public void setDauer(int dauer) {
+		this.dauer = dauer;
+	}
+
+	public boolean isIstWoechentlich() {
+		return istWoechentlich;
+	}
+
+	public void setIstWoechentlich(boolean istWoechentlich) {
+		this.istWoechentlich = istWoechentlich;
+	}
+	
+	public int getAktivitaetid() {
+		return aktivitaetid;
+	}
+
+	public void setAktivitaetid(int aktivitaetid) {
+		this.aktivitaetid = aktivitaetid;
+	}
+	
+}
